a6298a98b40fe77d4422274f1628a53a6223f8ae
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-15 06:45:40 +0500

    Update ass11

--- Abu_Hurera_Ass_Lac11.py
+++ Abu_Hurera_Ass_Lac11.py
@@ -24,18 +24,18 @@
 # emp.disply_detail()
 
 # Qno3
-class Math:
-   @staticmethod
-   def is_prime_num(num):
-      if num < 2:
-         return False
-      for i in range(2,int(num**0.5)+1):
-         if num % i ==0:
-            return False
-         return True
+# class Math:
+#    @staticmethod
+#    def is_prime_num(num):
+#       if num < 2:
+#          return False
+#       for i in range(2,int(num**0.5)+1):
+#          if num % i ==0:
+#             return False
+#          return True
 
-math = Math()
-print(math.is_prime_num(6))
+# math = Math()
+# print(math.is_prime_num(6))
 
 # Qno4
 # class Account:
