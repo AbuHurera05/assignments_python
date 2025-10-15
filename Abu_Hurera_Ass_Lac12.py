ebc8834a7a5d122960cf6c16d30e78ff28d678d2
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-15 19:18:59 +0500

    by adding Qno1 in lac12 assignment

--- Abu_Hurera_Ass_Lac12.py
+++ Abu_Hurera_Ass_Lac12.py
@@ -1,6 +1,17 @@
-class Vehicle:
-   def __init__(self,brand,model):
-      self.brand = brand
-      self.model = model
-   def show_detail(self):
-      return f"Brand: {self.brand} Model: {self.model}"
+# Qno1
+# class Vehicle:
+#    def __init__(self,brand,model):
+#       self.brand = brand
+#       self.model = model
+#    def show_detail(self):
+#       return f"Brand: {self.brand} Model: {self.model}"
+
+# class Motercycle(Vehicle):
+#    def __init__(self, brand, model,has_sidecar):
+#       super().__init__(brand, model)
+#       self.has_sidecar = has_sidecar
+
+# motercycle = Motercycle("Honda",2023,"bike")
+# detail = motercycle.show_detail()
+# print(f"{detail} {motercycle.has_sidecar}")
+
