439aa4fdc1203c78c94519031ca9bba4854007a7
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-15 06:28:17 +0500

    Update New Assignmet

--- Abu_Hurera_Ass_Lac11.py
+++ Abu_Hurera_Ass_Lac11.py
@@ -24,10 +24,15 @@
 # emp.disply_detail()
 
 # Qno3
-# class Math:
-#    @staticmethod
-#    def is_prime_num(num):
-#       if
+class Math:
+   @staticmethod
+   def is_prime_num(num):
+      if num < 2:
+         return False
+      for i in range(2,int(num**0.5)+1):
+         if num % i ==0:
+            return False
+         return True
 
 # Qno4
 # class Account:
