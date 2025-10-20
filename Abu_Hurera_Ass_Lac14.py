940927fc5f2eb53d7cff45ba37fd8b937c2ccdc9
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-20 21:01:22 +0500

    update the file

--- Abu_Hurera_Ass_Lac14.py
+++ Abu_Hurera_Ass_Lac14.py
@@ -1,15 +1,31 @@
+# FILE_PATH="input.txt"
+# COUNT_TXT_PATH="count.txt"
+# try:
+#    file = open(FILE_PATH,'r')
+#    line = file.readlines()
+#    count=0
+#    for i in line:
+#       print(i,end="")
+#       count+=1
+#    file.close
+#    count=str(count)
+#    file = open(COUNT_TXT_PATH,'w')
+#    count_text = file.write("Row in input.txt is: "+count)
+# except FileNotFoundError:
+#    print("File not found")
+
 FILE_PATH="input.txt"
 COUNT_TXT_PATH="count.txt"
 try:
-   file = open(FILE_PATH,'r')
-   line = file.readlines()
-   count=0
-   for i in line:
-      print(i,end="")
-      count+=1
+   with open(FILE_PATH,'r') as file:
+      line = file.readlines()
+      count=0
+      for i in line:
+         print(i,end="")
+         count+=1
    file.close
    count=str(count)
-   file = open(COUNT_TXT_PATH,'w')
-   count_text = file.write("Row in input.txt is: "+count)
+   with open(COUNT_TXT_PATH,'w') as file:
+      count_text = file.write("Row in input.txt is: "+count)
 except FileNotFoundError:
    print("File not found")
