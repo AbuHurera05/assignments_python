d8388b188dd56c0918e0ec7a9a3fb831e4ff472c
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-21 21:57:45 +0500

    Update my today assignmetn

--- Abu_Hurera_Ass_Lac15.py
+++ Abu_Hurera_Ass_Lac15.py
@@ -1,7 +1,10 @@
+import json
 student = [
    {"Name":"Abu Hurer","Age":23,"Marks":68},
-   {"Name":"Ahsan","Age":22,"Marks":88},
-   {"Name":"Ahsan","Age":22,"Marks":88},
-   {"Name":"Ahsan","Age":22,"Marks":88},
-   {"Name":"Ahsan","Age":22,"Marks":88}
+   {"Name":"Ahsan","Age":21,"Marks":88},
+   {"Name":"Ali Raza","Age":20,"Marks":69},
+   {"Name":"Muhammad","Age":19,"Marks":95},
+   {"Name":"Arsalan","Age":27,"Marks":60}
 ]
+with open("student.json","w") as file:
+   json_string = json.dump(student,file,indent=4)
