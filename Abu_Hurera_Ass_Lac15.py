fec5889df4c239dd0ccfb6195b03aefd017b41ff
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-21 21:49:46 +0500

    Today Assignmetn(21/10/2025)

---
+++ Abu_Hurera_Ass_Lac15.py
@@ -0,0 +1,7 @@
+student = [
+   {"Name":"Abu Hurer","Age":23,"Marks":68},
+   {"Name":"Ahsan","Age":22,"Marks":88},
+   {"Name":"Ahsan","Age":22,"Marks":88},
+   {"Name":"Ahsan","Age":22,"Marks":88},
+   {"Name":"Ahsan","Age":22,"Marks":88}
+]
