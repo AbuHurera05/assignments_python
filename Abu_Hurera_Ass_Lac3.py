aa5500ffbf4fbe55ea9e66f4990c761918031a6f
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-21 07:45:20 +0500

    update

---
+++ Abu_Hurera_Ass_Lac3.py
@@ -0,0 +1,106 @@
+# Qno1
+# n = int(input("Enter number: "))
+# if(n<=0):
+#    print("Invalid Number")
+# else:
+#    for i in range(1,n):
+#       print(i)
+#       if(i*3==i):
+#          continue
+#       else:
+#          print(i)
+#       if(i==50):
+#          break
+
+# _______________________Beginner Level______________________
+# Qno1
+# for i in range(11):
+#    print(i,end=" ")
+
+# Qno2
+print()
+# for i in range(11):
+#    if(i%2==0):
+#       print(i,end=" ")
+
+# Qno3
+# print()
+# for i in range(11):
+#    if(i%2==1):
+#       print(i,end=" ")
+
+# Qno4
+# print()
+# table=5
+# print(f"Table of {table}")
+# for i in range(1,11):
+#    print(f"{i} X {table} = {i*table}")
+   
+# Qno6
+# n= int(input("Enter a number: "))
+# sum=0
+# for i in range(1,n+1):
+#    sum+=i
+# print(sum)
+
+# Qno7
+# num=int(input("Enter a Number: "))
+# reverse=0
+# while num>0:
+#    mode = num%10
+#    reverse= reverse * 10 +mode
+#    num = num//10
+# print(f"Revers: {reverse}")
+
+# Qno8
+# n= int(input("Enter a number: "))
+# count=0
+# while(n>0):
+#    mod=n%10
+#    print(mod)
+#    count+=1
+#    n=n//10
+# print(count)
+
+# Qno9
+# text = input("Enter any text to count: ")
+# for i in text:
+#    print(i)
+
+# Qno10
+# n= int(input("Enter a number: "))
+# for i in range(n,1,-1):
+#    print(i)
+
+# ___________________________Intermediate Level________________________
+# Qno1
+# for i in range(1,101):
+#    if i%2==0:
+#       print(i)
+
+# Qno2
+# for i in range(1,101):
+#    if i%2==1:
+#       print(i)
+
+# Qno3
+# for i in range(1,101):
+#    if i%2==0:
+#       print(i)
+
+# Qno6
+# rows=5
+# for i in range(1,rows+1):
+#    for j in range(1,i+1):
+#       print("*",end="")
+#    print()
+
+   
+  
+
+
+
+   
+   
+
+
