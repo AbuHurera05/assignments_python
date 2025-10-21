06c6d178eee05225f280618eda84ed7128aaf10f
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-21 07:48:45 +0500

    update

---
+++ Abu_Hurera_Ass_Lac7.py
@@ -0,0 +1,54 @@
+# _______________________Practice Excercises 1_______________________
+# Qno1
+# list = [4,5,6,7,3]
+
+# Qno2
+# list2 = list[1:4]
+# square = []
+# for i in list2:
+#    square.append(i**2)
+# print(square)
+
+# Qno3
+# pack = ("Abu Hurera",23,"Tando Bago")
+# print(pack)
+# name,age,city = pack
+# print(name)
+# print(age)
+# print(city)
+
+# Qno4
+# tup = (4,5,2,5)
+# tup[2] = 6  #Error
+# list = [9,5,3,2,8,63,63]
+
+# tup2 = (list)
+# print(tup2)
+# list[0]=1
+# print(tup2)
+
+# ________________________________Practice Exercise 2_______________
+# Qno1
+# list = ["Lagaan","Sultan","Tere Nam","No you see me","Terminator"]
+# print(list)
+
+# Qno2
+# list[2] = "Harry Potter"
+# print(list)
+
+# Qno3
+# print(list[:2])
+
+# Qno4
+# sqaures = [x**2 for x in range(11)]
+# print(sqaures)
+
+# Qno5 pack
+# pack = ("Abu Hurera",23,"Tando Bago")
+# print(pack)
+
+# Qno6 unpack
+# name,age,city = pack
+# print(name)
+# print(age)
+# print(city)
