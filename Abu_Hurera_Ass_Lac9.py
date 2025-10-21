a8c8dfdeac87e55acf64b8d1b26bfd3749b332ef
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-21 07:50:04 +0500

    add ass lac9

---
+++ Abu_Hurera_Ass_Lac9.py
@@ -0,0 +1,20 @@
+# Qno1
+# name = input("Enter Your name: ")
+# print(name.upper())
+
+# Qno2
+# str = "banana"
+# print(str.count("a"))
+
+# Qno3
+# number = 1234.5678
+# print(f"Number: {number:.2f}")
+
+# Qno4
+# import re
+# str = "Order number: 12345, Date:2025-509-12"
+# digits = "".join(re.findall(r'\d',str))
+# print(digits)
+
+# Qno5
+# print(str.replace(" ","-"))
