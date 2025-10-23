42e6efc002fe387cb20abfaca0c513e2dfba6cf7
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-23 07:40:11 +0500

    Update Ass16

--- Abu_Hurera_Ass_Lac16.py
+++ Abu_Hurera_Ass_Lac16.py
@@ -1,12 +1,12 @@
 import requests
 
 # GET Request Practice
-names = ['Ali','Hamza','Sara','Ahmad']
+# names = ['Ali','Hamza','Sara','Ahmad']
 
-for name in names:
-   response = requests.get(f"https://api.agify.io?name={name}")
-   if response.status_code == 200:
-      data = response.json()
-      print(f"Age for name {name}: {data["age"]}")
-   else:
-      print("Faild request due to error")
+# for name in names:
+#    response = requests.get(f"https://api.agify.io?name={name}")
+#    if response.status_code == 200:
+#       data = response.json()
+#       print(f"Age for name {name}: {data["age"]}")
+#    else:
+#       print("Faild request due to error")
