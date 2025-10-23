4eb665a5f478d57c93fdd883f1b887c6977128d2
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-23 08:16:58 +0500

    Update

--- Abu_Hurera_Ass_Lac16.py
+++ Abu_Hurera_Ass_Lac16.py
@@ -30,33 +30,38 @@ import requests
 #    print("Details:", e)
 
 # API Cleint Mini Project
-# print("=== Weather API Client ===")
-# city = input("Enter a city name: ").strip()
+print("=== Weather API Client ===")
+print("You can check Weather of These Cities:\n(karchi,lahore,islamabad,tandobago,badin,golarchi,matli,talhar)")
+city = input("Enter a city name: ").strip()
 
-# # Simple city -> coordinates map (you can add more)
-# city_coords = {
-#     "karachi": {"lat": 24.8607, "lon": 67.0011},
-#     "lahore": {"lat": 31.5497, "lon": 74.3436},
-#     "islamabad": {"lat": 33.6844, "lon": 73.0479},
-#     "london": {"lat": 51.5072, "lon": -0.1276}
-# }
+# Simple city -> coordinates map (you can add more)
+city_coords = {
+    "karachi": {"lat": 24.8607, "lon": 67.0011},
+    "lahore": {"lat": 31.5497, "lon": 74.3436},
+    "islamabad": {"lat": 33.6844, "lon": 73.0479},
+    "tandobago": {"lat": 24.7887164, "lon": 68.9655333},
+    "badin": {"lat": 24.6557, "lon": 68.8372},
+    "matli": {"lat": 24.0444, "lon": 68.6589},
+    "talhar": {"lat": 24.84150, "lon": 68.83840},
+    "golarchi": {"lat": 24.6532363, "lon": 68.83840}
+}
 
-# if city.lower() in city_coords:
-#     lat = city_coords[city.lower()]["lat"]
-#     lon = city_coords[city.lower()]["lon"]
+if city.lower() in city_coords:
+    lat = city_coords[city.lower()]["lat"]
+    lon = city_coords[city.lower()]["lon"]
 
-#     weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
-#     response = requests.get(weather_url)
+    weather_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    response = requests.get(weather_url)
 
-#     if response.status_code == 200:
-#         data = response.json()
-#         current = data.get("current_weather", {})
+    if response.status_code == 200:
+        data = response.json()
+        current = data.get("current_weather", {})
 
-#         print(f"\nWeather in {city.title()}:")
-#         print(f"Temperature: {current.get('temperature', 'N/A')}°C")
-#         print(f"Windspeed: {current.get('windspeed', 'N/A')} km/h")
-#         print(f"Wind Direction: {current.get('winddirection', 'N/A')}°")
-#     else:
-#         print("Failed to fetch weather data.")
-# else:
-#     print("City not found in the list. Try Karachi, Lahore, Islamabad, or London.")
+        print(f"\nWeather in {city.title()}:")
+        print(f"Temperature: {current.get('temperature', 'N/A')}°C")
+        print(f"Windspeed: {current.get('windspeed', 'N/A')} km/h")
+        print(f"Wind Direction: {current.get('winddirection', 'N/A')}°")
+    else:
+        print("Failed to fetch weather data.")
+else:
+    print("City not found in the list. Try Karachi, Lahore, Islamabad, or Tanod Bago.")
