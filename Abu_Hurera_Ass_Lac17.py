d39dabb3a8db509a8e12f0bea395a6d572c80363
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-24 09:08:08 +0500

    Update

---
+++ Abu_Hurera_Ass_Lac17.py
@@ -0,0 +1,23 @@
+from datetime import *
+# Qno1
+# dob_str = input("Enter your date of birth (YYYY-MM-DD):")
+# dob = datetime.strptime(dob_str,"%Y-%m-%d").date()
+# today = datetime.today().date()
+# print(today)
+# days = (today-dob).days
+# second = (today-dob).total_seconds()
+# print(f"Days: {days}, Seconds: {second}")
+
+# Qno2
+# event_date = input("Enter your upcoming event date (YYYY-MM-DD HH:MM)")
+# event_date = datetime.strptime(event_date,"%Y-%m-%d %H:%M")
+# now = datetime.now()
+# time_left = event_date - now
+# days = time_left.days
+# hours = time_left.seconds // 3600
+# minutes = (time_left.seconds % 3600)//60
+# print(f"Time left for the event: {days} days, {hours} hours, {minutes} minutes")
+
+# Qno3
+
+
