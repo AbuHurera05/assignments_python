ee38c4961e281240b00248104a2075460fed1ea1
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-10-28 22:57:37 +0500

    update

--- Abu_Hurera_Ass_Lac19/app.py
+++ Abu_Hurera_Ass_Lac19/app.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 # --- Home Route ---
 @app.route('/')
 def home():
-    return "<h2>Welcome! Go to <a href='/login'>Login</a> or <a href='/feedback'>Feedback</a></h2>"
+    return render_template('home.html')
 
-# --- Login Form Route ---
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     message = ""
@@ -21,7 +20,6 @@ def login():
             message = "Error: Both username and password are required!"
     return render_template('login.html', message=message)
 
-# --- Feedback Form Route ---
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
     if request.method == 'POST':
@@ -31,13 +29,12 @@ def feedback():
         return render_template('feedback_result.html', name=name, email=email, message=message)
     return render_template('feedback.html')
 
-# --- Submit Route for GET and POST ---
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
     if request.method == 'POST':
-        return "<h3>Form submitted using POST method!</h3>"
+        return render_template('submit.html', method="POST")
     else:
-        return "<h3>You accessed /submit using GET method!</h3>"
+        return render_template('submit.html', method="GET")
 
 if __name__ == '__main__':
     app.run(debug=True)
